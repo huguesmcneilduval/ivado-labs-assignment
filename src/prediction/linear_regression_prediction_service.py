c804a803835c95b7089d66426dacb3ff8774acbf
Hugues McNeil-Duval <hugues.mcneil-duval@duvalhub.com>
2026-06-04 08:28:19 -0400

    Added Pearson correlation in PredictionService

--- src/prediction/linear_regression_prediction_service.py
+++ src/prediction/linear_regression_prediction_service.py
@@ -10,6 +10,7 @@ class LinearRegressionPredictionService(PredictionService):
         print('Starting training model...')
         self._model = LinearRegression()
         self._is_trained = False
+        self._correlation: float | None = None
 
         X: list[list[int]] = []
         y: list[int] = []
@@ -18,9 +19,12 @@ class LinearRegressionPredictionService(PredictionService):
             X.append([museum.city.population])
             y.append(museum.annual_visitor)
 
-        if X:
-            self._model.fit(np.array(X), np.array(y))
+        if len(X) >= 2:
+            X_array = np.array(X)
+            y_array = np.array(y)
+            self._model.fit(X_array, y_array)
             self._is_trained = True
+            self._correlation = float(np.corrcoef(X_array.flatten(), y_array)[0, 1])
         print('Model is trained')
 
     def predict(self, population: int) -> int:
@@ -29,3 +33,7 @@ class LinearRegressionPredictionService(PredictionService):
 
         prediction = self._model.predict(np.array([[population]]))[0]
         return int(prediction)
+
+    def correlation(self) -> float | None:
+        """Return the Pearson correlation between population and annual_visitor on the training data, or None if not enough data."""
+        return self._correlation
